ab742440534e98accefbd1b7992ce84fc16448fb
unknown <xrocketmanx007@gmail.com>
2018-10-22 20:40:04 +0300

    fix for deployment

--- client/src/app/url.constants.ts
+++ client/src/app/url.constants.ts
@@ -1,5 +1,5 @@
 import { environment } from '../environments/environment';
 
-export const BASE_URL: string = environment.production ? 'http://localhost:3000' : 'http://localhost:3000';
+export const BASE_URL: string = environment.production ? `http://${location.hostname}:3000` : `http://${location.hostname}:3000`;
 export const API_BASE_URL: string = `${BASE_URL}/api`;
 export const SOCKET_URL: string = BASE_URL.replace(/^http/, 'ws');
